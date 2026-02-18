7965777ea337f7815d6368a97aad06fb4d38d17c
ketevan20 <ketevansamukashvili@gmail.com>
2026-02-19 00:43:02 +0400

    fix animations

--- my-app/app/components/molecules/ExploreSection/ExploreSection.tsx
+++ my-app/app/components/molecules/ExploreSection/ExploreSection.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { motion } from 'motion/react'
+import Image from 'next/image'
 import React from 'react'
+import bg from '../../../../public/thinkerBackground.svg'
 
 const ExploreSection = ({ref}: any) => {
     return (
@@ -16,7 +18,8 @@ const ExploreSection = ({ref}: any) => {
                     <p className='text-[#FFB800] text-[16px] max-2xl:text-[12px] font-light'>From Early Civilizations to Modern Societies</p>
                     <p className='font-bold text-white text-[20px] max-2xl:text-[16px]'>Discover the Greatest Achievements</p>
                     <div className='relative min-w-55 max-w-[90%]'>
-                        <img src="thinkerBackground.svg" alt="" className='max-w-full' />
+                        {/* <img src="thinkerBackground.svg" alt="" className='max-w-full' /> */}
+                        <Image width={225} height={365} src={bg} alt='Thinker Background' className="max-w-full" priority/>
                         <img src="thinker.svg" alt="" className='absolute bottom-0' />
                     </div>
                     <div className='absolute bottom-4 left-2 right-2 text-center font-sans'>
@@ -25,7 +28,7 @@ const ExploreSection = ({ref}: any) => {
                     </div>
                 </motion.div>
 
-                <motion.div initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x:0}} transition={{duration: 1, ease: 'anticipate', delay: 1}} viewport={{once: true}} className='relative overflow-hidden z-10 sm:col-span-2 bg-linear-to-b from-black to-black/0 backdrop-blur-sm px-5 py-4 rounded-2xl border-[0.4px] border-[#FFB800]'>
+                <motion.div initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x:0}} transition={{duration: 1, ease: 'anticipate'}} viewport={{once: true}} className='relative overflow-hidden z-10 sm:col-span-2 bg-linear-to-b from-black to-black/0 backdrop-blur-sm px-5 py-4 rounded-2xl border-[0.4px] border-[#FFB800]'>
                     <img src="tshirts.svg" alt="" className='absolute z-10 right-0 bottom-0 max-h-full' />
                     <div className='relative z-20'>
                         <div className='font-sans text-white'>
@@ -36,7 +39,7 @@ const ExploreSection = ({ref}: any) => {
                     </div>
                 </motion.div>
 
-                <motion.div initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x:0}} transition={{duration: 1, ease: 'anticipate', delay: 1}} viewport={{once: true}} className='relative z-10 sm:col-span-2 bg-linear-to-b from-black to-black/0 backdrop-blur-sm px-5 py-4 rounded-2xl border-[0.4px] border-[#FFB800] font-sans text-white'>
+                <motion.div initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x:0}} transition={{duration: 1, ease: 'anticipate'}} viewport={{once: true}} className='relative z-10 sm:col-span-2 bg-linear-to-b from-black to-black/0 backdrop-blur-sm px-5 py-4 rounded-2xl border-[0.4px] border-[#FFB800] font-sans text-white'>
                     <h1 className='text-[26px] max-2xl:text-[20px] font-bold max-md:text-[16px] mb-2'>Be Part of the Journey</h1>
                     <p  className='text-[16px] max-2xl:text-[14px] font-light max-md:text-[14px] mb-2'>Connect with like-minded individuals. Share your thoughts, participate in discussions,</p>
                 </motion.div>
