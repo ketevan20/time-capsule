59fb1b2fa153b1b8271a0af9092d3d2782a6152f
ketevan20 <ketevansamukashvili@gmail.com>
2026-02-18 20:27:41 +0400

    fix: ui

--- my-app/app/components/molecules/AboutSection/AboutSection.tsx
+++ my-app/app/components/molecules/AboutSection/AboutSection.tsx
@@ -9,10 +9,10 @@ const AboutSection = ({ref}: any) => {
             <motion.h1 initial={{opacity: 0, x: 100}} whileInView={{opacity: 1, x:0}} transition={{duration: 1, ease: 'anticipate'}} viewport={{once: true}} className='font-against text-[64px] max-2xl:text-[32px] max-sm:text-[24px] self-end mt-16 max-md:mt-8'>Tracing Our<br/> Milestones</motion.h1>
             <motion.p initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x:0}} transition={{duration: 1, ease: 'anticipate'}} viewport={{once: true}}  className='underline text-[24px] font-normal max-2xl:text-[16px] cursor-pointer max-sm:text-[10px]'>Read More</motion.p>
         </div>
-        <motion.img initial={{opacity: 0, y: 100}} whileInView={{opacity: 1, y:0}} transition={{duration: 1, ease: 'anticipate', delay: 0.1}} viewport={{once: true, amount: 0.2}} src="base.svg" alt="base" className='absolute z-10 bottom-0'/>
-        <motion.img initial={{opacity: 0, y: 100}} whileInView={{opacity: 1, y:0}} transition={{duration: 1, ease: 'anticipate', delay: 0.2}} viewport={{once: true, amount: 0.2}} src="base2.svg" alt="base" className='absolute z-10 bottom-0'/>
-        <motion.img initial={{opacity: 0, y: 100}} whileInView={{opacity: 1, y:0}} transition={{duration: 1, ease: 'anticipate', delay: 0.3}} viewport={{once: true, amount: 0.2}} src="base3.svg" alt="base" className='absolute z-10 bottom-0'/>
-        <motion.img initial={{opacity: 0, y: 100}} whileInView={{opacity: 1, y:0}} transition={{duration: 1, ease: 'anticipate', delay: 0.4}} viewport={{once: true, amount: 0.2}} src="base4.svg" alt="base" className='absolute z-10 bottom-0'/>
+        <motion.img initial={{y: 100}} whileInView={{y:0}} transition={{duration: 1, ease: 'anticipate', delay: 0.1}} viewport={{once: true, amount: 0.2}} src="base.svg" alt="base" className='absolute z-10 bottom-0'/>
+        <motion.img initial={{y: 100}} whileInView={{y:0}} transition={{duration: 1, ease: 'anticipate', delay: 0.2}} viewport={{once: true, amount: 0.2}} src="base2.svg" alt="base" className='absolute z-10 bottom-0'/>
+        <motion.img initial={{y: 100}} whileInView={{y:0}} transition={{duration: 1, ease: 'anticipate', delay: 0.3}} viewport={{once: true, amount: 0.2}} src="base3.svg" alt="base" className='absolute z-10 bottom-0'/>
+        <motion.img initial={{y: 100}} whileInView={{y:0}} transition={{duration: 1, ease: 'anticipate', delay: 0.4}} viewport={{once: true, amount: 0.2}} src="base4.svg" alt="base" className='absolute z-10 bottom-0'/>
     </div>
   )
 }
